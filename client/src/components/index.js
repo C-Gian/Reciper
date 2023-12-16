6e17814eced9ec163654973e209c7dfc79ab02f9
C-Gian <gianluca.e.lav@gmail.com>
2023-12-16 14:51:01 +0100

    aaaaaaaaaaa

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import { LeftBar } from "./LeftBar/LeftBar";
 import { RecipePost } from "./RecipePost/RecipePost";
 import { Header } from "./Header/Header";
+import { RecipeModal } from "./RecipeModal/RecipeModal";
 
-export { LeftBar, RecipePost, Header };
+export { LeftBar, RecipePost, Header, RecipeModal };
