68c623f7fba3a92e214f61cc28481850733f996c
C-Gian <gianluca.e.lav@gmail.com>
2023-12-25 20:40:01 +0100

    aaaaaaaaaa

--- client/src/components/RecipePost/RecipePost.jsx
+++ client/src/components/RecipePost/RecipePost.jsx
@@ -42,7 +42,8 @@ export const RecipePost = ({ recipe, index, likedRecipes, savedRecipes }) => {
     document.body.style.overflow = "auto";
   };
 
-  const updateLikes = async () => {
+  const updateLikes = async (e) => {
+    e.stopPropagation();
     if (isLiked) {
       try {
         await axios.put(
@@ -94,7 +95,8 @@ export const RecipePost = ({ recipe, index, likedRecipes, savedRecipes }) => {
     }
   };
 
-  const updateSaved = async () => {
+  const updateSaved = async (e) => {
+    e.stopPropagation();
     if (isSaved) {
       try {
         await axios.put(
@@ -148,28 +150,16 @@ export const RecipePost = ({ recipe, index, likedRecipes, savedRecipes }) => {
 
   return (
     <div>
-      <div className="recipepost-container">
+      <div className="recipepost-container" onClick={openModal}>
         <div className="rp-t">
-          <Link
-            className="rpt-t"
-            to={{
-              pathname: "/recipe",
-            }}
-            state={recipe}
-          >
+          <div className="rpt-t">
             <img src={imageUrl} alt={title} />
-          </Link>
+          </div>
           <div className="rpt-b">
             <Link to="/profile" className="rptb-img">
               <img src="\assets\images\recipecard\profile_icon.png" alt="" />
             </Link>
-            <Link
-              className="rptb-infos"
-              to={{
-                pathname: "/recipe",
-              }}
-              state={recipe}
-            >
+            <div className="rptb-infos">
               <h1>{title}</h1>
               <div className="rptbi-rating">
                 <Rating
@@ -181,7 +171,7 @@ export const RecipePost = ({ recipe, index, likedRecipes, savedRecipes }) => {
                 <label>{Math.floor(ratingVote * 10) / 10}</label>
                 <label>{`(${totalRatings})`}</label>
               </div>
-            </Link>
+            </div>
           </div>
         </div>
         <div className="rp-b">
@@ -228,15 +218,6 @@ export const RecipePost = ({ recipe, index, likedRecipes, savedRecipes }) => {
             <label className={`rpbi-like ${isLiked ? "rpbi-liked" : ""}`}>
               {likes}
             </label>
-          </div>
-          <div className="rpb-divider"></div>
-          <div className="rpb-item rpbi-comments" onClick={openModal}>
-            <svg viewBox="0 0 24 24" aria-hidden="true" width={20} fill="white">
-              <g>
-                <path d="M1.751 10c0-4.42 3.584-8 8.005-8h4.366c4.49 0 8.129 3.64 8.129 8.13 0 2.96-1.607 5.68-4.196 7.11l-8.054 4.46v-3.69h-.067c-4.49.1-8.183-3.51-8.183-8.01zm8.005-6c-3.317 0-6.005 2.69-6.005 6 0 3.37 2.77 6.08 6.138 6.01l.351-.01h1.761v2.3l5.087-2.81c1.951-1.08 3.163-3.13 3.163-5.36 0-3.39-2.744-6.13-6.129-6.13H9.756z"></path>
-              </g>
-            </svg>
-            <label>{comments}</label>
           </div>
           <div className="rpb-divider"></div>
           <div className="rpb-item" onClick={updateSaved}>
